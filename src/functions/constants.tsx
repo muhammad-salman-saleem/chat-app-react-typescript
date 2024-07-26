29c55ae6c897e38bbe3974f09a0c1d5b861850b8
muhammad-salman-saleem <msalman1221998@gmail.com>
2024-07-26 23:25:23 +0500

    update constest

--- src/functions/constants.tsx
+++ src/functions/constants.tsx
@@ -9,3 +9,11 @@ export const privateKey: string = process.env.REACT_APP_PROJECT_KEY
 export const newsApi = process.env.REACT_APP_NEWS_API
   ? process.env.REACT_APP_NEWS_API
   : "";
+
+export const rapidNewsApi = process.env.REACT_APP_RAPID_API
+  ? process.env.REACT_APP_RAPID_API
+  : "";
+
+export const rapidNewsApiHost = process.env.REACT_APP_RAPID_API_HOST
+  ? process.env.REACT_APP_RAPID_API_HOST
+  : "";
